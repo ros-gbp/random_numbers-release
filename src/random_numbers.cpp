c86f9797ae5bbde9cbce99d6a2bbc9f406c89322
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2024-08-12 13:11:08 +0200

    Rebase from 'debian/noetic/random_numbers'

--- src/random_numbers.cpp
+++ src/random_numbers.cpp
@@ -1,36 +1,36 @@
 /*********************************************************************
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2008, Willow Garage, Inc.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
 
 /* Author: Ioan Sucan */
 
@@ -49,9 +49,10 @@ static boost::uint32_t first_seed_ = 0;
 static boost::uint32_t firstSeed(void)
 {
   boost::scoped_ptr<int> mem(new int());
-  first_seed_ = (boost::uint32_t)((boost::posix_time::microsec_clock::universal_time() -
-                            boost::posix_time::ptime(boost::date_time::min_date_time)).total_microseconds() +
-                           (unsigned long long)(mem.get()));
+  first_seed_ = (boost::uint32_t)(
+      (boost::posix_time::microsec_clock::universal_time() - boost::posix_time::ptime(boost::date_time::min_date_time))
+          .total_microseconds() +
+      (unsigned long long)(mem.get()));
   return first_seed_;
 }
 
@@ -63,27 +64,23 @@ static boost::uint32_t nextSeed(void)
   static boost::mutex rngMutex;
   boost::mutex::scoped_lock slock(rngMutex);
   static boost::lagged_fibonacci607 sGen(firstSeed());
-  static boost::uniform_int<>       sDist(1, 1000000000);
+  static boost::uniform_int<> sDist(1, 1000000000);
   static boost::variate_generator<boost::lagged_fibonacci607&, boost::uniform_int<> > s(sGen, sDist);
   boost::uint32_t v = s();
   return v;
 }
 
-
-random_numbers::RandomNumberGenerator::RandomNumberGenerator(void) : generator_(nextSeed()),
-                                                                     uniDist_(0, 1),  
-                                                                     normalDist_(0, 1),
-                                                                     uni_(generator_, uniDist_),
-                                                                     normal_(generator_, normalDist_)
+random_numbers::RandomNumberGenerator::RandomNumberGenerator(void)
+  : generator_(nextSeed())
+  , uniDist_(0, 1)
+  , normalDist_(0, 1)
+  , uni_(generator_, uniDist_)
+  , normal_(generator_, normalDist_)
 {
 }
 
 random_numbers::RandomNumberGenerator::RandomNumberGenerator(boost::uint32_t seed)
-                                                                   : generator_(seed),
-                                                                     uniDist_(0, 1),
-                                                                     normalDist_(0, 1),
-                                                                     uni_(generator_, uniDist_),
-                                                                     normal_(generator_, normalDist_)
+  : generator_(seed), uniDist_(0, 1), normalDist_(0, 1), uni_(generator_, uniDist_), normal_(generator_, normalDist_)
 {
   // Because we manually specified a seed, we need to save it ourselves
   first_seed_ = seed;
@@ -95,7 +92,8 @@ void random_numbers::RandomNumberGenerator::quaternion(double value[4])
 {
   double x0 = uni_();
   double r1 = sqrt(1.0 - x0), r2 = sqrt(x0);
-  double t1 = 2.0 * boost::math::constants::pi<double>() * uni_(), t2 = 2.0 * boost::math::constants::pi<double>() * uni_();
+  double t1 = 2.0 * boost::math::constants::pi<double>() * uni_(),
+         t2 = 2.0 * boost::math::constants::pi<double>() * uni_();
   double c1 = cos(t1), s1 = sin(t1);
   double c2 = cos(t2), s2 = sin(t2);
   value[0] = s1 * r1;
